f188c882a68db7957ca8183efa1ff354f844ae5f
harsh30896 <hars30896@gmail.com>
2023-03-07 18:44:25 +0530

    test commit 2

---
+++ src/StringPractice/Remove_the_occurence.java
@@ -0,0 +1,28 @@
+package StringPractice;
+
+public class Remove_the_occurence {
+
+    public static String remove(String  input)
+    {
+        StringBuilder sb = new StringBuilder();
+        int i = 0;
+        while (i < input.length()) {
+            if (i < input.length() - 1 && input.charAt(i) == 'A' && input.charAt(i + 1) == 'B') {
+                i += 2;
+            } else if (input.charAt(i) == 'C') {
+                i++;
+            } else {
+                sb.append(input.charAt(i));
+                i++;
+            }
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        String str = "ABCACBCAABB";
+        System.out.println("Before the removing the element of str "+str);
+        String str1= remove(str);
+        System.out.println("After removing the element of str " +str1);
+    }
+}
